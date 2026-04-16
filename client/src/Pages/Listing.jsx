3187de3864aca912fd6abf7f9f2ca83e4f3a2fcf
Bhhgtr <himandithb@gmail.com>
2026-04-16 10:38:11 +0530

    Add contact owner functionality to the listing page

--- client/src/Pages/Listing.jsx
+++ client/src/Pages/Listing.jsx
@@ -6,6 +6,7 @@ import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
 import { FaShare, FaMapMarkerAlt, FaGasPump, FaCar } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
+import Contact from '../components/Contact';
 
 export default function Listing() {
   SwiperCore.use([Navigation]);
@@ -13,6 +14,7 @@ export default function Listing() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [copied, setCopied] = useState(false);
+   const [contact, setContact] = useState(false); 
   const { currentUser } = useSelector((state) => state.user);
   const params = useParams();
 
@@ -128,6 +130,12 @@ export default function Listing() {
                 {listing.fuelType.charAt(0).toUpperCase() + listing.fuelType.slice(1)}
               </li>
             </ul>
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button onClick={()=>setContact(true)} className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>
+                Contact landlord
+              </button>
+            )}
+            {contact && <Contact listing={listing}/>}
           </div>
         </div>
       )}
