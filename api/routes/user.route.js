dc6f30b9c71cea23a090878c1e39779e53234da3
Bhhgtr <himandithb@gmail.com>
2026-04-17 12:39:28 +0530

    Update user routes and improve listing price calculations

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,16 +1,17 @@
 import express  from "express";
-import {deleteUser, test, updateUser, getUserListing, getUsers} from '../controllers/user.controller.js';
+import {deleteUser, test, updateUser, getUserListings, getUser} from '../controllers/user.controller.js';
 import { verifyToken } from "../utils/verifyUser.js";
 import { getPresignedUrl } from '../controllers/s3.controller.js';
 
 const router = express.Router();
 
 router.get('/test', test);
+router.get('/s3-url', verifyToken, getPresignedUrl);
 router.post('/update/:id', verifyToken, updateUser);
 router.delete('/delete/:id', verifyToken, deleteUser);
 router.get('/listings/:id', verifyToken, getUserListings);
 router.get('/:id', verifyToken, getUser);
-router.get('/s3-url', verifyToken, getPresignedUrl);
+
 
 
 export default router;
